5477cd2b9fb54ea8ace4ef43beb6970b9264eb79
username <spiridonovfed@yandex.ru>
2021-04-13 15:07:59 +0300

    Change test_task04.py in order to meet 'black' criterias

--- homework1/task04/tests/test_task04.py
+++ homework1/task04/tests/test_task04.py
@@ -8,4 +8,9 @@ def test_positive_case1():
 
 def test_positive_case2():
     """Testing that function finds number of tuples correctly"""
-    assert check_sum_of_four([-12, 55, 11, 4, 10], [8, 3], [-10, 7, 33], [19, 41, 37, -40, -3]) == 4
+    assert (
+        check_sum_of_four(
+            [-12, 55, 11, 4, 10], [8, 3], [-10, 7, 33], [19, 41, 37, -40, -3]
+        )
+        == 4
+    )
