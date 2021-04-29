1d17860556938376888ce9ae85a7c1f07be639a9
username <spiridonovfed@yandex.ru>
2021-04-29 10:19:29 +0300

    correct task02 to mmet Pylint criteria

--- homework1/task02.py
+++ homework1/task02.py
@@ -13,7 +13,8 @@ from typing import Sequence
 def check_fibonacci(data: Sequence[int]) -> bool:
     """ Checking if sequence given is a fibonacci sequence """
 
-    # checking if the first number in sequence is fibonacci number - function returns "False" if it's not:
+    # checking if the first number in sequence is fibonacci number -
+    # function returns "False" if it's not:
     if not data[0] == 0:
         if not (
             sqrt(5 * (data[0] ** 2) - 4) % 1 == 0
@@ -21,7 +22,8 @@ def check_fibonacci(data: Sequence[int]) -> bool:
         ):
             return False
 
-    # checking if the second number in sequence is fibonacci number - function returns "False" if it's not:
+    # checking if the second number in sequence is fibonacci number -
+    # function returns "False" if it's not:
     if not (
         sqrt(5 * (data[1] ** 2) - 4) % 1 == 0 or sqrt(5 * (data[1] ** 2) + 4) % 1 == 0
     ):
