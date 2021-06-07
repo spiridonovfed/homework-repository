a68a5e3360e0f9baff02a5e05166bf18fb345a40
username <spiridonovfed@yandex.ru>
2021-06-07 14:23:46 +0300

    solutions of tasks of homework11

--- homework11/hw1.py
+++ homework11/hw1.py
@@ -33,3 +33,22 @@ class SizesEnum(metaclass=SimplifiedEnum):
 assert ColorsEnum.RED == "RED"
 assert SizesEnum.XL == "XL"
 """
+
+
+class SimplifiedEnum(type):
+    """Metaclass to store variables passed in in __key attribute of instance-class
+    class ColorsEnum(metaclass=SimplifiedEnum):
+        __keys = ("RED", "BLUE", "ORANGE", "BLACK")
+
+    assert ColorsEnum.RED == "RED"
+    assert ColorsEnum.ORANGE == "ORANGE"
+    """
+
+    def __init__(cls, name, bases, dct):
+        super(SimplifiedEnum, cls).__init__(name, bases, dct)
+
+    def __getattr__(cls, item):
+        if item in cls.__dict__[f"_{cls.__name__}__keys"]:
+            return item
+        else:
+            raise KeyError("This key is not declared in the class's __key attribute")
--- homework11/hw2.py
+++ homework11/hw2.py
@@ -26,5 +26,54 @@ order_1 = Order(100, morning_discount)
 assert order_1.final_price() == 50
 
 order_2 = Order(100, elder_discount)
-assert order_1.final_price() == 10
+assert order_2.final_price() == 10
 """
+from abc import ABCMeta, abstractmethod
+
+
+def to_camel_case(snake_str):
+    components = snake_str.split("_")
+    # We capitalize the first letter of each component except the first one
+    # with the 'title' method and join them together.
+    return "".join(x.title() for x in components)
+
+
+#####################################################
+
+
+class DiscountStrategy(metaclass=ABCMeta):
+    """Abstract class for different discount strategies"""
+
+    @property
+    @abstractmethod
+    def discount(self) -> float:
+        pass
+
+
+class MorningDiscount(DiscountStrategy):
+    discount = 0.5
+
+
+class ElderDiscount(DiscountStrategy):
+    discount = 0.9
+
+
+class StudentDiscount(DiscountStrategy):
+    discount = 0.15
+
+
+class Order:
+    """Class takes initial price and discount_strategy function,
+    that has a name corresponding to one of the determined DiscountStrategy classes.
+    final_price method returns final price after discount application"""
+
+    def __init__(self, price, discount_strategy):
+        # find corresponding DiscountStrategy class by name of a function passed in
+        strategy_class_name = to_camel_case(discount_strategy.__qualname__)
+        strategy = globals()[strategy_class_name]
+
+        self.price = price
+        self.discount = strategy.discount
+
+    def final_price(self):
+        return self.price - self.price * self.discount
